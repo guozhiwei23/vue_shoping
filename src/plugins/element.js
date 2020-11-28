eac37493ffcc1b9be169d2fb60ed0c02c99944fc
guozhiwei <401062848@qq.com>
2020-11-28 15:03:37 +0800

    主页结构

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, container, header, aside, main } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(container)
+Vue.use(header)
+Vue.use(aside)
+Vue.use(main)
 Vue.prototype.$message = Message
